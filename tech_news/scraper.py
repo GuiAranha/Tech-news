7bfe095c0117807d58255936103eba4a98e66b85
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-11-09 15:39:40 -0300

    feat: create scrape_novidades function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -17,7 +18,9 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    list = selector.css(".cs-overlay-link::attr(href)").getall()
+    return list
 
 
 # Requisito 3
